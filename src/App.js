1cf0a905ac1097c15bb3d944ad48a6b85f3f68e5
Gautam <gautamkumar929982@gmail.com>
2024-07-20 23:38:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Header from './MyContent/Header';
+function App() {
+  return (
+    <>
+    <h1>This is my first react website</h1>
+    <Header/>
+    </>
+  );
+}
+export default App;
---
+++ src/MyContent/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+export default function Header() {
+  return (
+    <>
+    <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>services</li>
+    </nav>
+    </>
+  )
+}
